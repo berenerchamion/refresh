f6c82087bf91270555c74f2629bb90035045f08d
Beren Erchamion <beren@houseofbeor.net>
2018-03-21 18:27:00 -0400

    post voting working on the details page! Refresh still works too!!

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,16 @@
 import { fetchPosts,
         fetchPost,
         fetchCategories,
+        votePost,
+        editPost,
+        addPost,
       } from '../utils/ReadableAPI'
 
 export const FETCH_POSTS = 'FETCH_POSTS'
 export const FETCH_POST = 'FETCH_POST'
+export const VOTE_FOR_POST = 'VOTE_FOR_POST'
+export const ADD_POST = 'ADD_POST'
+export const EDIT_POST = 'EDIT_POST'
 
 export const fetchAllPosts = () => {
   return (dispatch) => {
@@ -22,6 +28,27 @@ export const fetchOnePost = (id) => {
     })
   }
 }
+
+export const voteForPost = (postData) => {
+    return (dispatch) => {
+      votePost(postData)
+      dispatch({type: VOTE_FOR_POST, postData })
+    }
+  }
+
+  export const addNewPost = (postData, cb) => {
+    return (dispatch) => {
+      addPost(postData).then(() => cb())
+      dispatch({ type: ADD_POST, postData })
+    }
+  }
+
+  export const submitEdit = (postData, cb) => {
+    return (dispatch) => {
+      editPost(postData).then(() => cb())
+      dispatch({type: EDIT_POST, postData})
+    }
+  }
 
 //Categories related APIs
 export const FETCH_CATEGORIES = 'FETCH_CATEGORIES'
--- src/reducers/post.js
+++ src/reducers/post.js
@@ -1,15 +1,39 @@
 import { FETCH_POSTS,
-          FETCH_POST
+          FETCH_POST,
+          VOTE_FOR_POST,
+          ADD_POST,
+          EDIT_POST,
         } from '../actions'
 
 function posts(state=[], action) {
-  const { posts, post } = action
+  const { posts, post, postData } = action
 
   switch(action.type) {
     case FETCH_POSTS:
       return posts.filter(post => !(post.deleted))
     case FETCH_POST:
       return post
+    case ADD_POST:
+    return state.concat([postData])
+    case EDIT_POST:
+      return state.map(post => {
+        if (post.id === postData.id){
+          post = postData
+        }
+        return post
+      })
+    case VOTE_FOR_POST:
+      return state.map(post => {
+        if (post.id === action.postData.postId) {
+          if (action.postData.voteType === "upVote") {
+            post.voteScore += 1
+          }
+          else if (action.postData.voteType === "downVote") {
+            post.voteScore -= 1
+          }
+        }
+        return post
+      })
     default:
       return state
   }
