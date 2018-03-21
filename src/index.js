0eb35345c6a23f0694a93dcc5bf8d73abc43f21a
Beren Erchamion <beren@houseofbeor.net>
2018-03-20 21:20:27 -0400

    Setting up the basics.

--- src/index.js
+++ src/index.js
@@ -4,5 +4,37 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { createStore, applyMiddleware, compose } from 'redux'
+import { BrowserRouter } from 'react-router-dom'
+import reducer from './reducers'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
+const logger = store => next => action => {
+  console.group(action.type)
+  console.info('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  console.groupEnd(action.type)
+  return result
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(
+  reducer,
+  composeEnhancers(
+    applyMiddleware(thunk, logger)
+  )
+)
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('root'));
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
